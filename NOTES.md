5a6858ab7a78a8eb8a8bdb92bc39f24c22eeb9ac
Akash Nair <akash.nair@sparwelt.de>
2020-01-13 15:10:46 +0100

    Added error handling in test script. Finalized K8S YAML. Added run instructions

--- NOTES.md
+++ NOTES.md
@@ -4,3 +4,10 @@
 3. Tried to map the host ports of 9933, 9944 and 30333 to the docker container and tried again. But still no go. 
 4. Searched the Github issues page for substrate and found `https://github.com/paritytech/substrate/issues/439` which directed me to PR #495 which indicated to use the flags `--ws-external` and `--rpc-external`
 5. Added `-r` flag to jq command for raw hex output instead of string
+
+## Run-steps
+1. Run `kubectl apply -f k8s.yaml` to create the Deployment with one Pod and the Service
+2. Copy the URL of the Service and pass it to the test script as follows: `bash test.sh load-balancer-url.cloud.com`
+3. If the block height is already above 10, the script exits with a non-zero code
+4. Once the block height crosses 10, the script exits with a zero code
+
--- test.sh
+++ test.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+# Get the hostname as command line argument
+host=$1
+
+# Test host reachability
+if curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "chain_getBlock"}' -s $host:9933; then
+  echo "Server reachable"
+else
+  echo "Unable to reach host. Please check again"
+  exit 0
+fi
+
+# Function that stores the block height in Hex into $hexHeight and then parses it into Int and stores it in $height
+
 getBlockHeight () {
 
-hexHeight=`curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "chain_getBlock"}' localhost:9933 | jq -r .result.block.header.number`
+hexHeight=`curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "chain_getBlock"}' $host:9933 | jq -r .result.block.header.number`
 
 height=$(( $hexHeight ))
+
 }
 
+# Invoke the function to initialise the variables
 getBlockHeight
-echo $height
 
+# Verify if block height has already surpassed 10
 if [ $height -gt 10 ]
   then
     echo "Block height already above 10"
